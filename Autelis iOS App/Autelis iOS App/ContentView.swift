55ddc5c19f7e02e49734e409269940520cf9d333
Matthew Brennan <matthew.brennan@me.com>
2023-11-05 14:52:13 -0600

    Sample Pool Filter Slider

--- Autelis iOS App/Autelis iOS App/ContentView.swift
+++ Autelis iOS App/Autelis iOS App/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 import SwiftData
 
+
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
 
+    @State private var fruits = [
+        "Apple",
+        "Banana",
+        "Papaya",
+        "Mango"
+    ]
+    
+    @State private var poolFilter = false
+
+    
     var body: some View {
         Text("Hello Kara Smell like Roses and Poo!")
-        }
+        Toggle(isOn: $poolFilter) {
+                Text("Pool Filter")
+            }
+    }
     }
 
 
